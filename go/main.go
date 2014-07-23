dfd0f6038d0aa930bddba6d5517e9c7ce29910b0
Toomore Chiang <toomore0929@gmail.com>
2014-07-23 21:52:33 +0800

    Using flag.

--- go/main.go
+++ go/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 type slackbot struct {
@@ -12,15 +14,22 @@ type slackbot struct {
 }
 
 func (bot *slackbot) send(channel, text string) {
-	var url_path string
-	url_path = fmt.Sprintf("https://%s.slack.com/services/hooks/slackbot?token=%s&channel=%s", bot.team, bot.token, channel)
-
+	params := url.Values{}
+	params.Add("token", bot.token)
+	params.Add("channel", channel)
+	url_path := fmt.Sprintf("https://%s.slack.com/services/hooks/slackbot?%s",
+		bot.team, params.Encode())
 	fmt.Println(url_path)
 	body := bytes.NewBufferString(text)
 	http.Post(url_path, "text/plain", body)
 }
 
 func main() {
-	bot := slackbot{"pinkoi", "..."}
-	bot.send("@toomore", "Hello, Toomore [from golang]")
+	team := flag.String("team", "pinkoi", "Team name")
+	token := flag.String("token", "...", "Slack token")
+	text := flag.String("text", "Hello Wrold! [from golang]", "Say something...")
+	channel := flag.String("chnl", "@toomore", "Send to who ...")
+	flag.Parse()
+	bot := slackbot{*team, *token}
+	bot.send(*channel, *text)
 }
