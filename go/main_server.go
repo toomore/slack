79fb8945e66c9d1eb328566bc8e3af649bf29d5c
Toomore Chiang <toomore0929@gmail.com>
2014-07-24 23:18:51 +0800

    Add HasPrefix.

--- go/main_server.go
+++ go/main_server.go
@@ -2,16 +2,19 @@ package main
 
 import "net/http"
 import "fmt"
+import "strings"
 
 func index(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintln(writer, request)
 	if request.Method == "POST" {
 		request.ParseForm()
 		fmt.Println(request.Form)
-		bot := slackbot{"pinkoi", "..."}
-		channel := fmt.Sprintf("%s%s", "#", request.Form["channel_name"][0])
-		text := fmt.Sprintf("@%s: %s [from golang bot]", request.Form["user_name"][0], request.Form["text"][0])
-		bot.send(channel, text)
+		if strings.HasPrefix(request.Form["text"][0], "ok go") {
+			bot := slackbot{"pinkoi", "..."}
+			channel := fmt.Sprintf("%s%s", "#", request.Form["channel_name"][0])
+			text := fmt.Sprintf("@%s: %s [from golang bot]", request.Form["user_name"][0], request.Form["text"][0][len("ok go "):])
+			bot.send(channel, text)
+		}
 	}
 }
 
