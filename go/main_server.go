60521f430a87a6b00b6ba0e8923ac440cf5cf736
Toomore Chiang <toomore0929@gmail.com>
2014-07-25 20:42:18 +0800

    Add switch.

--- go/main_server.go
+++ go/main_server.go
@@ -5,10 +5,11 @@ import "net/http"
 import "net/url"
 import "strings"
 
-func BotSend(form url.Values) {
+func BotSend(formdata *url.Values, msg string) {
+	form := *formdata
 	bot := slackbot{"pinkoi", "..."}
-	channel := fmt.Sprintf("%s%s", "#", form["channel_name"][0])
-	text := fmt.Sprintf("@%s: %s [from golang bot]", form["user_name"][0], form["text"][0][len("ok go "):])
+	channel := fmt.Sprintf("%s%s", "#", form.Get("channel_name"))
+	text := fmt.Sprintf("@%s: %s [from golang bot]", form.Get("user_name"), msg)
 	bot.send(channel, text)
 }
 
@@ -17,8 +18,12 @@ func index(writer http.ResponseWriter, request *http.Request) {
 	if request.Method == "POST" {
 		request.ParseForm()
 		fmt.Println(request.Form)
-		if strings.HasPrefix(request.Form["text"][0], "ok go") {
-			BotSend(request.Form)
+		switch {
+		case strings.HasPrefix(request.Form.Get("text"), "ok go Hello"):
+			BotSend(&request.Form, request.Form.Get("text")[len("ok go "):])
+
+		case strings.HasPrefix(request.Form.Get("text"), "ok go time"):
+			BotSend(&request.Form, request.Form.Get("text")[len("ok go "):])
 		}
 	}
 }
