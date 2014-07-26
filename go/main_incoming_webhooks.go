a4f13ee4e0c70ceffd7277c46f48f01f0bef75b3
Toomore Chiang <toomore0929@gmail.com>
2014-07-26 09:59:01 +0800

    Add bot profile.

--- go/main_incoming_webhooks.go
+++ go/main_incoming_webhooks.go
@@ -16,11 +16,13 @@ type IncomingWebHooks struct {
 func (bot *IncomingWebHooks) send(channel, text string) {
 	params := url.Values{}
 	params.Add("token", bot.token)
-	params.Add("channel", channel)
 	url_path := fmt.Sprintf("https://%s.slack.com/services/hooks/incoming-webhook?%s",
 		bot.team, params.Encode())
 	text_map := make(map[string]string)
 	text_map["text"] = text
+	text_map["channel"] = channel
+	text_map["username"] = "PinkoiBot [go]"
+	text_map["icon_emoji"] = ":mypinkoi:"
 	text_json, _ := json.Marshal(text_map)
 	body := bytes.NewBufferString(fmt.Sprintf("%s", text_json))
 	http.Post(url_path, "text/plain", body)
