0b0ddb615d7eb0da95543b454aa5e5e6a1f03256
Toomore Chiang <toomore0929@gmail.com>
2014-07-26 10:51:28 +0800

    Add `Post`.

--- go/main_incoming_webhooks.go
+++ go/main_incoming_webhooks.go
@@ -14,17 +14,23 @@ type IncomingWebHooks struct {
 }
 
 func (bot *IncomingWebHooks) send(channel, text string) {
-	params := url.Values{}
-	params.Add("token", bot.token)
-	url_path := fmt.Sprintf("https://%s.slack.com/services/hooks/incoming-webhook?%s",
-		bot.team, params.Encode())
-	text_map := make(map[string]string)
+	text_map := make(map[string]interface{})
 	text_map["text"] = text
 	text_map["channel"] = channel
 	text_map["username"] = "PinkoiBot [go]"
 	text_map["icon_emoji"] = ":mypinkoi:"
+
+	bot.Post(text_map)
+}
+
+func (bot *IncomingWebHooks) Post(text_map map[string]interface{}) {
 	text_json, _ := json.Marshal(text_map)
 	body := bytes.NewBufferString(fmt.Sprintf("%s", text_json))
+
+	params := url.Values{}
+	params.Add("token", bot.token)
+	url_path := fmt.Sprintf("https://%s.slack.com/services/hooks/incoming-webhook?%s",
+		bot.team, params.Encode())
 	http.Post(url_path, "text/plain", body)
 }
 
@@ -43,7 +49,7 @@ func (bot *IncomingWebHooks) renderAtta(fallback, text, title, value string) map
 
 func main() {
 	bot := IncomingWebHooks{"pinkoi", "..."}
-	//bot.send("@toomore", "From IncomingWebHooks.")
+	bot.send("@toomore", "From IncomingWebHooks.")
 	//j, _ := json.Marshal(bot.renderAtta("fallback", "text", "title", "value"))
 	//fmt.Printf("%s", j)
 }
