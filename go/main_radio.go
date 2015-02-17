28fcb0f8692f0e92e1bb6aecff42200b1b570027
Toomore Chiang <toomore0929@gmail.com>
2015-02-17 11:07:23 +0000

    Add some go files.

---
+++ go/main_radio.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	//"bufio"
+	"bytes"
+	"fmt"
+	"net/http"
+	"os"
+	//"os/exec"
+	"regexp"
+	//"strings"
+	"text/tabwriter"
+)
+
+func GetUrl(url_no string) (replace_string string) {
+	url := fmt.Sprintf("http://hichannel.hinet.net/radio/mobile/index.do?id=%s", url_no)
+	//resp, _ := http.Get("http://hichannel.hinet.net/radio/mobile/index.do?id=232")
+	//resp, _ := http.Get("http://hichannel.hinet.net/radio/mobile/index.do?id=207")
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+	html := new(bytes.Buffer)
+	html.ReadFrom(resp.Body)
+	//fmt.Println(html.String())
+	reg := regexp.MustCompile(`var url = '([\S]+)'`)
+	url_string := reg.FindAllStringSubmatch(html.String(), -1)
+	//fmt.Println(url_string[0][1])
+
+	replace := regexp.MustCompile(`\\\/`)
+	replace_string = replace.ReplaceAllString(url_string[0][1], `/`)
+	//fmt.Println(replace_string)
+	return
+}
+
+func GetList() {
+	resp, _ := http.Get("http://hichannel.hinet.net/radio/mobile/index.do?id=207")
+	defer resp.Body.Close()
+	html := new(bytes.Buffer)
+	html.ReadFrom(resp.Body)
+	reg := regexp.MustCompile(`<div class="stationName">(.+)</div>[\s]+<div class="list"><a href="#" onclick="getInfo\('list','([\d]+)'\);return false;"></a></div>`)
+	url_string := reg.FindAllStringSubmatch(html.String(), -1)
+	url_string = append(url_string, []string{"", "中廣新聞網", "207"})
+	//fmt.Println(url_string)
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+	var output string
+	for no, data := range url_string {
+		output += fmt.Sprintf("%d. [%s] %s\t", no+1, data[2], data[1])
+		if (no+1)%3 == 0 {
+			fmt.Fprintln(w, output)
+			output = ""
+		}
+	}
+	fmt.Fprintln(w, output)
+	w.Flush()
+}
+
+func PrintChannel() {
+	fmt.Println("[207] 中廣新聞網")
+	fmt.Println("[205] 中廣流行網 i like")
+	fmt.Println("[206] 中廣音樂網i radio")
+	fmt.Println("[232] 飛碟電台")
+	fmt.Println("[222] HitFm聯播網 Taipei 北部")
+	fmt.Println("[156] KISS RADIO 大眾廣播電台")
+	fmt.Println("[308] KISS RADIO 網路音樂台")
+	fmt.Println("[187] NEWS98新聞網")
+	fmt.Println("[370] POP Radio 台北流行廣播電台")
+}
+
+//func main() {
+//	//fmt.Println(GetUrl("207"))
+//	//PrintChannel()
+//	GetList()
+//
+//	//in := bufio.NewReader(os.Stdin)
+//	//std_string, _ := in.ReadString('\n')
+//	//radio_url := GetUrl(strings.Split(std_string, "\n")[0])
+//	//fmt.Println(radio_url)
+//	//exec.Command("/Applications/VLC.app/Contents/MacOS/VLC", radio_url).Start()
+//}
---
+++ go/main_server_2.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+import "net/http"
+import "net/url"
+import "strings"
+
+func BotSend(formdata *url.Values, msg string) {
+	form := *formdata
+	bot := slackbot{"pinkoi", "..."} // in Slackbot
+	channel := fmt.Sprintf("%s%s", "#", form.Get("channel_name"))
+	text := fmt.Sprintf("@%s: %s [from golang bot]", form.Get("user_name"), msg)
+	bot.send(channel, text)
+}
+
+func index(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintln(writer, request)
+	if request.Method == "POST" {
+		request.ParseForm()
+		fmt.Println(request.Form)
+		switch {
+		case strings.HasPrefix(request.Form.Get("text"), "ok go Hello"):
+			BotSend(&request.Form, request.Form.Get("text")[len("ok go "):])
+
+		case strings.HasPrefix(request.Form.Get("text"), "ok go time"):
+			BotSend(&request.Form, request.Form.Get("text")[len("ok go "):])
+
+		case strings.HasPrefix(request.Form.Get("text"), "ok go radio"):
+            radio_url := GetUrl(request.Form.Get("text")[len("ok go radio"):])
+			//BotSend(&request.Form, request.Form.Get("text")[len("ok go radio"):])
+            BotSend(&request.Form, fmt.Sprintf("```%s```", radio_url))
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/slack", index)
+	http.ListenAndServe(":9001", nil)
+}
